3672a7e1038add86e2797f8b6de8f66c2b7fedc7
faza Fahamsyah <fazafahamsyah@gmail.com>
2017-08-29 06:32:51 +0700

    Redux is hard!

--- playgrounds/more_redux.js
+++ playgrounds/more_redux.js
@@ -1,20 +1,55 @@
 const reducer = (state=[], action) => {
-    if (action.type === 'split_string'){
-      return action.payload.split('');
-    }
-    return state;
+  if (action.type === 'split_string'){
+    return action.payload.split('');
+  } else if (action.type === 'add_character'){
+    // state.push(action.payload);
+    // return state;
+
+    // es6:
+
+    return [ ...state, action.payload ];
   }
   
-  const store = Redux.createStore(reducer);
-  
-  store.getState();
-  
-  
-  const action = { 
-        type: 'Split_string', 
-      payload: 'asdf'
-  };
-  
-  store.dispatch(action);
-  
-  store.getState();
+  return state;
+}
+
+const store = Redux.createStore(reducer);
+
+store.getState();
+
+
+const action = { 
+  	type: 'split_string', 
+    payload: 'asdf'
+};
+
+store.dispatch(action);
+
+store.getState();
+
+const action2 = { 
+  	type: 'add_character', 
+    payload: 'a'
+};
+
+
+store.dispatch(action2)
+
+store.getState();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
